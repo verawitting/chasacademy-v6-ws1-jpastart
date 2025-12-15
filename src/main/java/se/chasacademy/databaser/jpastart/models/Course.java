e1e2b0f918cf2ed2e80bc46945561cd490ec94e3
Vera Witting <verawitting@gmail.com>
2025-12-15 13:57:54 +0100

    filled classes

--- src/main/java/se/chasacademy/databaser/jpastart/models/Course.java
+++ src/main/java/se/chasacademy/databaser/jpastart/models/Course.java
@@ -1,4 +1,50 @@
 package se.chasacademy.databaser.jpastart.models;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+    @Column(name = "CourseCode")
+    private String courseCode;
+
+    private List<Student> students = new ArrayList();
+
+    public Course(int id, String title, String courseCode) {
+        this.id = id;
+        this.title = title;
+        this.courseCode = courseCode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
 }
--- src/main/java/se/chasacademy/databaser/jpastart/models/Teacher.java
+++ src/main/java/se/chasacademy/databaser/jpastart/models/Teacher.java
@@ -1,4 +1,49 @@
 package se.chasacademy.databaser.jpastart.models;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+    @Column(name = "email")
+    private String email;
+
+    // listan av kurser i konstruktorn?
+    public Teacher(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
